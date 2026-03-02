4a79105e04fd3cf98e9a5461fb34e52b1da061ee
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-03-02 20:08:22 +0530

    Implement get all todos api

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -50,3 +50,60 @@ func CreateTodo(pool *pgxpool.Pool, title string, completed bool) (*models.Todo,
 	// Return the created todo
 	return &todo, nil
 }
+
+func GetAllTodos(pool *pgxpool.Pool) ([]models.Todo, error) {
+	var ctx context.Context
+	var cancel context.CancelFunc
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+		SELECT id, title, completed, created_at, updated_at
+		FROM todos
+		ORDER BY created_at DESC
+	`
+
+	var rows, err = pool.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	// Slice to store all todos
+	var todos []models.Todo = []models.Todo{}
+
+	/*
+		Next() :
+			rows contains multiple records from the database.
+			But Go does not give you all rows at once.
+			Instead, you must iterate over them one by one — like reading lines from a file.
+	*/
+	// Loop through result rows
+	for rows.Next() {
+		var todo models.Todo
+
+		// Scan row data into todo struct fields
+		err = rows.Scan(
+			&todo.ID,
+			&todo.Title,
+			&todo.Completed,
+			&todo.CreatedAt,
+			&todo.UpdatedAt,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Add todo to slice
+		todos = append(todos, todo)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	// Return all todos
+	return todos, nil
+}
